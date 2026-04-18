165ed2b83ba6eba0104890256b6eea51d15ea19b
DaniAndradeDash <daniandrademty@gmail.com>
2026-04-18 17:32:01 -0600

    agrgando videos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Distintivo from "@/sections/Distintivo";
 import Asesorias from "@/sections/Asesorias";
 import Compromiso from "@/sections/Compromiso";
 import Contacto from "@/sections/Contacto";
+import Videos from "@/sections/Videos";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Servicios />
       <Distintivo />
       <Asesorias />
+      <Videos />
       <Compromiso />
       <Contacto />
     </>
